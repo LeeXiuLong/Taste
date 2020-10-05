373fed706c98820e3fc5a15df6eb4d4cc0e6652a
Jourdan Ooi <jungterooi@gmail.com>
2020-10-05 12:44:25 -0400

    all our files are now in github

---
+++ frontend/frontend/src/components/splash/splash.jsx
@@ -0,0 +1,15 @@
+class SplashPage extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <h1>Mern Junipers</h1>
+                <footer>
+                    Copyright &copy; 2020 Junipers
+        </footer>
+            </div>
+        );
+    }
+}
+
+export default SplashPage;
