cf5f2971dfc013ed713fb82f4bdd3fdf5f0f8a5b
ellisPae <ellis.h.pae@gmail.com>
2020-10-05 21:58:33 -0400

    Add gitignore

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,4 @@
-// module.exports = {
-//     mongoURI = 'mongodb+srv://dev:6ZozC5LoQKUjhDyu@mernjuniper.cowg6.mongodb.net/MERNJUNIPER?retryWrites=true&w=majority',
-//     secretOrKey: "secret"
-// }
+
 
 module.exports = {
     mongoURI: 'mongodb+srv://dev:6ZozC5LoQKUjhDyu@mernjuniper.cowg6.mongodb.net/MERNJUNIPER?retryWrites=true&w=majority',
