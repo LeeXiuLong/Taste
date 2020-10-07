e76933a3ae06221add6a88ae57de298431cbf006
nahid <thisisnahid@gmail.com>
2020-10-06 20:56:12 -0400

    frontend components added

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -1,17 +1,25 @@
 import React from 'react';
-import { AuthRoute } from '../util/route_util';
+import { AuthRoute, ProtectedRoute } from '../util/route_util';
 import Splash from './splash/splash_container';
 import UserShow from './user_profile/user_show_container';
 import Modal from './modal/modal';
 import Home from './home/home_container';
+import ListIndex from './list/list_index_container';
+import ListForm from './list/list_form/list_form_container';
+import RestaurantIndex from './restaurant/restaurant_index_container';
+import RestaurantForm from './restaurant/restaurant_form/restaurant_form_container';
 // import { Switch } from 'react-router-dom';
 
 const App = () => (
     <div>
         <Modal />
         <AuthRoute exact path="/" component={Splash} />
-        <AuthRoute path="/users" component={UserShow} />
-        <AuthRoute path="/home" component={Home}/>
+        <ProtectedRoute path="/users/:userId" component={UserShow} />
+        <ProtectedRoute path="/home" component={Home}/>
+        <ProtectedRoute path="/:userId/lists" component={ListIndex} />
+        <ProtectedRoute path="/create-list" component={ListForm}/>
+        <ProtectedRoute path="/:userId/restaurants" component={RestaurantIndex} />
+        <ProtectedRoute path='/add-restaurant' component={RestaurantForm}/>
     </div>
 );
 
--- frontend/src/components/user_profile/user_show.jsx
+++ frontend/src/components/user_profile/user_show.jsx
@@ -5,25 +5,7 @@ import './user_show.scss'
 class UserShow extends React.Component {
 
     render() {
-        let list1, list2, list3;
-
-        list1 = (
-            <div className="list-container">
-                <p>KBBQ Spots</p>
-            </div>
-        )
-
-        list2 = (
-            <div className="list-container">
-                <p>Ice Cream</p>
-            </div>
-        )
-
-        list3 = (
-            <div className="list-container">
-                <p>Late Nights</p>
-            </div>
-        )
+        const { currentUser } = this.props;
 
         return (
             <div>
@@ -31,7 +13,7 @@ class UserShow extends React.Component {
                 <div className="user-show-container">
                     <div className="user-show-subcontainer">
                         <div className='us-header-container'>
-                            <h1>Nahid</h1>
+                            <h1>{currentUser.name}</h1>
                             <div className="us-insights">
                                 <ul className="insights-ul-us">
                                     <li className="following-us-insights">
@@ -44,17 +26,7 @@ class UserShow extends React.Component {
                                     </li>
                                 </ul>
                             </div>
-                        </div>
-                        <div className="us-bottom-container">
-                            <div className="list-container">
-                                <h2>lists</h2>
-                                <div className="list-subcontainer">
-                                    <ul>
-                                        <li>{list1}</li>
-                                        <li>{list2}</li>
-                                        <li>{list3}</li>
-                                    </ul>
-                                </div>
+
                                 <div className="bottom-containter">
                                     <div className="recs-header">
                                     </div>
@@ -68,16 +40,15 @@ class UserShow extends React.Component {
                                     <h1>updates</h1>
                                 </div>
                             </div>
-                        </div>
                     </div>
-                    <footer className="us-footer">
-                        <div className="footer-links-us">
-                            <a href="https://github.com/LeeXiuLong/Taste">Github</a>
-                        </div>
-                        <div className="copyright-us">
-                            Copyright &copy; 2020 Junipers
-                        </div>
-                    </footer>
+                        <footer className="us-footer">
+                            <div className="footer-links-us">
+                                <a href="https://github.com/LeeXiuLong/Taste">Github</a>
+                            </div>
+                            <div className="copyright-us">
+                                Copyright &copy; 2020 Junipers
+                            </div>
+                        </footer>
                 </div>
             </div >
         )
