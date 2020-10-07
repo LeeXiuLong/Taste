0fa0b09db3a476835209f315119f5212a09482a0
Justin Cho <jchoustin91@gmail.com>
2020-10-06 19:29:29 -0700

    Changed lines on app.js

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const bodyParser = require('body-parser');
 const passport = require('passport');
 
 const users = require("./routes/api/users");
+
 const lists = require("./routes/api/lists");
 
 
@@ -24,8 +25,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use("/api/users", users);
-app.use("/api/lists", lists); 
 
+app.use("/api/lists", lists); 
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
