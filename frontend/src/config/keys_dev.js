e70eb12c25ffbe606e63c6c8cb972e16ae58a214
nahid <thisisnahid@gmail.com>
2020-10-09 13:20:53 -0400

    frontend changes

---
+++ frontend/src/config/keys_dev.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://dev:6ZozC5LoQKUjhDyu@mernjuniper.cowg6.mongodb.net/MERNJUNIPER?retryWrites=true&w=majority',
+    secretOrKey: "secret",
+    googleApiKey: "AIzaSyAlTG4pWd_djWygNL4N8oKmZIJnhj4BB5o"
+}
